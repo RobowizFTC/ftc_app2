a92b9c09dc9fdde6308bde9fa5381653bc9c9c06
AdarshK1 <addy2999@gmail.com>
2016-02-18 22:25:31 -0500

    asdfads

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AccelerometerComponent.java
@@ -0,0 +1,215 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.SystemClock;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.LegacyModule;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Handler;
+
+/**
+ * An op mode that uses the geomagnetic and accelerometer values to calculate device
+ * orientation and return those values in telemetry.
+ * It makes use of getRotationMatrix() and getOrientation(), but does not use
+ * remapCoordinateSystem() which one might want.
+ * see: http://developer.android.com/reference/android/hardware/SensorManager.html#remapCoordinateSystem(float[], int, int, float[])
+ */
+public class AccelerometerComponent extends OpMode implements SensorEventListener {
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
+    DcMotor frontLeftDrive;
+    DcMotor frontRightDrive;
+    DcMotor tape;
+    Servo adjust;
+    Servo deposit;
+    LegacyModule legacyModule;
+    UltrasonicSensor ultraLeft;
+    UltrasonicSensor ultraRight;
+    private String startDate;
+    private SensorManager mSensorManager;
+    private Sensor accelerometer;
+    private Sensor magnetometer;
+
+    // orientation values
+    private float azimuth = 0.0f;      // value in radians
+    private float pitch = 0.0f;        // value in radians
+    private float roll = 0.0f;         // value in radians
+
+    private float[] mGravity;       // latest sensor values
+    private float[] mGeomagnetic;   // latest sensor values
+    private boolean run = true;
+
+    /*
+    * Constructor
+    */
+    public AccelerometerComponent() {
+
+    }
+    public void forward(double val) {
+        frontLeftDrive.setPower(val);
+        frontRightDrive.setPower(val);
+        backLeftDrive.setPower(val);
+        backRightDrive.setPower(val);
+    }
+    public void backwards(double val) {
+        frontLeftDrive.setPower(-val);
+        frontRightDrive.setPower(-val);
+        backLeftDrive.setPower(-val);
+        backRightDrive.setPower(-val);
+    }
+    public void turnLeft(double val) {
+        frontLeftDrive.setPower(-val);
+        frontRightDrive.setPower(val);
+        backLeftDrive.setPower(-val);
+        backRightDrive.setPower(val);
+    }
+    public void turnRight(double val) {
+        frontLeftDrive.setPower(val);
+        frontRightDrive.setPower(-val);
+        backLeftDrive.setPower(val);
+        backRightDrive.setPower(-val);
+    }
+    public void stopDrive(){
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+    }
+    public void fullStop(){
+        stopDrive();
+        tape.setPower(0);
+        //adjust.setPower(0);
+    }
+    /*
+    * Code to run when the op mode is first enabled goes here
+    * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#init()
+    */
+    @Override
+    public void init() {
+        backRightDrive = hardwareMap.dcMotor.get("backRightDrive");
+        backLeftDrive = hardwareMap.dcMotor.get("backLeftDrive");
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive = hardwareMap.dcMotor.get("frontRightDrive");
+        frontLeftDrive = hardwareMap.dcMotor.get("frontLeftDrive");
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        tape = hardwareMap.dcMotor.get("tape");
+        adjust = hardwareMap.servo.get("adjust");
+        deposit = hardwareMap.servo.get("deposit");
+
+        mSensorManager = (SensorManager) hardwareMap.appContext.getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        azimuth = 0.0f;      // value in radians
+        pitch = 0.0f;        // value in radians
+        roll = 0.0f;
+    }
+
+    /*
+* Code to run when the op mode is first enabled goes here
+* @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+*/
+    @Override
+    public void start() {
+        startDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
+        // delay value is SENSOR_DELAY_UI which is ok for telemetry, maybe not for actual robot use
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    public void sleep(long time){
+        try { this.wait(time);}
+        catch (Exception ex) { android.util.Log.d("YourApplicationName", ex.toString()); }
+    }
+
+    /*
+    * This method will be called repeatedly in a loop
+    * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+    */
+    @Override
+    public void loop() {
+//        telemetry.addData("1 Start", "OrientOp started at " + startDate);
+//        telemetry.addData("2 note1", "values below are in degrees" );
+//        telemetry.addData("3 note2", "azimuth relates to magnetic north" );
+//        telemetry.addData("4 note3", " " );
+//        if (azimuth < 0.5){
+//            forward(1);
+//        }
+//        else{
+//            stopDrive();
+//        }
+
+        if (run) {
+            backwards(.5);
+            sleep(2000);
+            stopDrive();
+            sleep(250);
+
+            forward(.5);
+            sleep(1000);
+            stopDrive();
+            sleep(250);
+
+            turnLeft(.5);
+            sleep(700);
+            stopDrive();
+            sleep(250);
+
+            forward(.3);
+            sleep(3000);
+            stopDrive();
+            sleep(250);
+            telemetry.addData("stuff", "asdfadfasdfasdfasdf");
+            run = false;
+        }
+        telemetry.addData("azimuth", Math.round(Math.toDegrees(azimuth)));
+        telemetry.addData("pitch", Math.round(Math.toDegrees(pitch)));
+        telemetry.addData("roll", Math.round(Math.toDegrees(roll)));
+    }
+
+    /*
+    * Code to run when the op mode is first disabled goes here
+    * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
+    */
+    @Override
+    public void stop() {
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // not sure if needed, placeholder just in case
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        // we need both sensor values to calculate orientation
+        // only one value will have changed when this method called, we assume we can still use the other value.
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            mGravity = event.values;
+        }
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            mGeomagnetic = event.values;
+        }
+        if (mGravity != null && mGeomagnetic != null) {  //make sure we have both before calling getRotationMatrix
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                azimuth = orientation[0]; // orientation contains: azimuth, pitch and roll
+                pitch = orientation[1];
+                roll = orientation[2];
+            }
+        }
+    }
+}
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankDriveClimberLinearUltrasonic.java
@@ -0,0 +1,156 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+
+import android.hardware.Sensor;
+
+import com.qualcomm.robotcore.hardware.LegacyModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.LegacyModule;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+import java.util.UnknownFormatConversionException;
+
+/**
+ * Created by ayylmao on 11/22/15.
+ */
+public class TankDriveClimberLinearUltrasonic extends LinearOpMode {
+
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
+    DcMotor frontLeftDrive;
+    DcMotor frontRightDrive;
+    DcMotor tape;
+    Servo adjust;
+    Servo deposit;
+    LegacyModule legacyModule;
+    UltrasonicSensor ultraLeft;
+    UltrasonicSensor ultraRight;
+    public void forward(double val) {
+        frontLeftDrive.setPower(val);
+        frontRightDrive.setPower(val);
+        backLeftDrive.setPower(val);
+        backRightDrive.setPower(val);
+    }
+    public void backwards(double val) {
+        frontLeftDrive.setPower(-val);
+        frontRightDrive.setPower(-val);
+        backLeftDrive.setPower(-val);
+        backRightDrive.setPower(-val);
+    }
+    public void turnLeft(double val) {
+        frontLeftDrive.setPower(-val);
+        frontRightDrive.setPower(val);
+        backLeftDrive.setPower(-val);
+        backRightDrive.setPower(val);
+    }
+    public void turnRight(double val) {
+        frontLeftDrive.setPower(val);
+        frontRightDrive.setPower(-val);
+        backLeftDrive.setPower(val);
+        backRightDrive.setPower(-val);
+    }
+
+
+    public void stopDrive(){
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+    }
+
+    public void fullStop(){
+        stopDrive();
+        tape.setPower(0);
+        //adjust.setPower(0);
+    }
+
+
+    public boolean seeRamp(UltrasonicSensor ultraLeft, UltrasonicSensor ultraRight){
+        telemetry.addData("ultraL", "Left: " + ultraLeft.getUltrasonicLevel());
+        telemetry.addData("ultraR", "Right: " + ultraRight.getUltrasonicLevel());
+
+        double diff = Math.abs(ultraLeft.getUltrasonicLevel() - ultraRight.getUltrasonicLevel());
+        if( diff <= 5)
+            return true;
+        return false;
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        backRightDrive = hardwareMap.dcMotor.get("backRightDrive");
+        backLeftDrive = hardwareMap.dcMotor.get("backLeftDrive");
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive = hardwareMap.dcMotor.get("frontRightDrive");
+        frontLeftDrive = hardwareMap.dcMotor.get("frontLeftDrive");
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        tape = hardwareMap.dcMotor.get("tape");
+        adjust = hardwareMap.servo.get("adjust");
+        deposit = hardwareMap.servo.get("deposit");
+        //legacyModule = hardwareMap.legacyModule.get("legacy");
+        //ultraLeft = hardwareMap.ultrasonicSensor.get("ultraLeft");
+        //ultraRight = hardwareMap.ultrasonicSensor.get("ultraRight");
+        waitForStart();
+
+        deposit.setPosition(Servo.MAX_POSITION);
+
+//        boolean seen = false;
+//        forward(.60);
+//        sleep(1000);
+//
+//        turnLeft(.60);
+//        while(!seen) {
+//            seen = seeRamp(ultraLeft, ultraRight);
+//        }
+//        stopDrive();
+
+        backwards(.5);
+        sleep(2500);
+        stopDrive();
+        sleep(250);
+
+        forward(.5);
+        sleep(700);
+        stopDrive();
+        sleep(250);
+
+
+        turnLeft(.5);
+        sleep(900);
+        stopDrive();
+        sleep(250);
+
+        forward(.5);
+        sleep(1000);
+        stopDrive();
+        sleep(250);
+
+        tape.setPower(1);
+        sleep(3500);
+        tape.setPower(0);
+        sleep(250);
+
+        adjust.setPosition(Servo.MIN_POSITION);
+        sleep(500);
+        tape.setPower(-.5);
+        forward(1);
+        sleep(700);
+        tape.setPower(0);
+        stopDrive();
+        sleep(250);
+
+    }
+}
